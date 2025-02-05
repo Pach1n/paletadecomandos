b30c26cc77dd1e229e34566a708d6441978d8142
Pach1n <ivanx.afonso@gmail.com>
2025-02-05 13:30:46 +0100

    Update App.java

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         //CACOTA //"234" //"gfshtfd"
+        //TUNUNC
     }
 
 }
